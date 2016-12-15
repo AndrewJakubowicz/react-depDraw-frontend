32d0000cbc81e42dcc4167b33d43d7e3769f07f6
SpyR1014 <spyr1014@gmail.com>
2016-12-15 17:04:32 +1100

    Set up some static views

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import {CodeView} from './components/codeView';
+
+import './App.css'
+
+class Heading extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h2>MVP: Dependency navigator</h2>
+      </div>
+    )
+  }
+}
+
+
+function BodyWrapper(props) {
+  return (
+    <div className="App-body">
+      {props.children}
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Heading/>
+        <BodyWrapper>
+          <CodeView/>
+        </BodyWrapper>
       </div>
     );
   }
---
+++ src/components/codeView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export class CodeView extends React.Component {
+    render(){
+        return (
+            <p>Code View</p>
+        )
+    }
+}
