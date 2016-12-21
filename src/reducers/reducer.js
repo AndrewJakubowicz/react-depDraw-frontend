538acbb3f741e67f0f38b2af3b9057a7ac938a94
SpyR1014 <spyr1014@gmail.com>
2016-12-21 20:32:41 +1100

    refactored reducers

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,22 +2,28 @@ import * as Redux from 'redux';
 
 import * as a from '../actions/actions';
 
+import {combineReducers} from 'redux';
+
 import * as Immutable from 'immutable';
 
 
 // State required in the app.
 // file.ts -> all the tokens.
 // map of file -> tokens
-const fileTokenReducer = (state = { tokenList: [] }, action) => {
+const fileTokenReducer = (state = [], action) => {
     switch (action.type){
         case a.RECEIVE_FILE_TOKEN_DATA:
-            return {
-                tokenList: action.tokenList
-            };
+            return action.tokenList;
         default:
             return state;
     }
 }
 
+export const getFileTokens = (state) => {
+    return state.tokenList;
+}
+
 // export const rootReducer = Redux.combineReducers(fileTokenReducer);
-export const rootReducer = fileTokenReducer;
+export const rootReducer = combineReducers({
+    tokenList: fileTokenReducer
+});
